f8e3219affa3eef09fe5e96effd7fd13ab9866d7
Dalvelac <dalvelac@myuax.com>
2024-11-05 11:02:49 +0100

    Ejercicio3.cpp

---
+++ Ejercicio 3/Ejercicio3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int calificaciones[8];
+    int suma = 0;
+
+    for (int i = 0; i < 8; ++i) { // Vamos a utilizar un array: esto es mucho mas ordenador que tener 8 variables.
+        std::cout << "Introduce la calificacion del ejercicio " << i + 1 << ": ";
+        std::cin >> calificaciones[i];
+        if (calificaciones[i] < 0 || calificaciones[i] > 10) {
+            std::cout << "La calificacion debe ser un numero entre 0 y 10" << std::endl;
+            return 0;
+        }
+        suma += calificaciones[i];
+    }
+
+    double promedio = static_cast<double>(suma) / 8; // Hacemos un cast a double para que la division sea decimal.
+    std::cout << "El promedio de las calificaciones es: " << promedio << std::endl;
+
+    return 0;
+}
