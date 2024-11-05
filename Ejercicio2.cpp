4f38adf06f90ba6b361a66e3c22b4173c12c613d
Daniel Alves Lacambra <dalvelac@myuax.com>
2024-11-05 11:21:46 +0100

    Update Ejercicio2.cpp

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 
-// Crea un programa en C++ que solicite al usuario ingresar un número del 1 al 7. El programa deberá imprimir el nombre del día de la semana correspondiente al número ingresado (por ejemplo, 1 para lunes, 2 para martes, ..., 7 para domingo).
-
-#include <iostream>
-
 int main() {
     int dia;
 
